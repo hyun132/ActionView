4b25e23d326affdcfaea84e116ac7f16121158b5
hyun <rlaalgus132@naver.com>
2020-03-04 15:41:37 +0900

    Search View 사용해보기

---
+++ app/src/main/java/com/example/actionview/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.actionview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.main_menu,menu)
+
+        return true
+    }
+}
